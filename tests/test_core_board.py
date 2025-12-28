3b6a4bb7098f442697730b4cf99d92db8c5d58a2
D4R102004 <alfonsodario54@gmail.com>
2025-12-27 19:36:34 -0500

    test(test_core_board): added test for a second node search, as well as another test for Cost Sensitive class
    introduced lines in the test_grid_initialization for testing the second children, as well as the cost sensitive
    
    Closes #2

--- tests/test_core_board.py
+++ tests/test_core_board.py
@@ -1,14 +1,14 @@
 import pytest
-from pathos.core import Node, SearchDomain, GoalOriented
+from pathos.core import Node, SearchDomain, GoalOriented, CostSensitive
 
 
 # A simple grid 8x8
-class Grid(GoalOriented[tuple[int, int], str]):
-    # A simple 10x10 grid to have a labyrinth
-    def __init__(self, rows: int = 10, cols: int = 10):
+class Grid(GoalOriented[tuple[int, int], str], CostSensitive[tuple[int, int], str]):
+    # A simple 10x10 grid to have a labyrinth, moving it can cost more than 1.0 per step.
+    def __init__(self, rows: int = 10, cols: int = 10, cost: float = 1.0):
         self.rows = rows
         self.cols = cols
-        
+        self.cost = cost
 
     def actions(self, state: tuple[int, int]):
         # the available movements are up, down, left, right
@@ -69,10 +69,24 @@ class Grid(GoalOriented[tuple[int, int], str]):
         :param state: The state we're in currently
         """
         return state == (self.rows, self.cols)
+    
+    def step_cost(self, state: tuple[int, int], action: str, next_state: tuple[int, int]) -> float:
+        """
+        Docstring for step_cost
+        
+        In this simple grid, every movement has the same cost.
+
+        :param self: The grid
+        :param state: The current state
+        :param action: The action taken
+        :param next_state: The resulting state
+        :return: The cost of taking the action
+        """
+        return self.cost
      
 def test_grid_initialization():
     # Test that expand and initialize gets correct nodes
-    problem = Grid()
+    problem = Grid(cost = 2.0)
     root = Node(state = (1, 1))
 
     children = root.expand(problem)
@@ -86,12 +100,24 @@ def test_grid_initialization():
     assert child_down.state == (2, 1)
     assert child_down.action == 'down'
     assert child_down.depth == 1
-    assert child_down.path_cost == 1.0
+    assert child_down.path_cost == 2.0
 
     # Second movement should be right if you are at the beggining
     child_right = children[1]
     assert child_right.action == 'right'
     assert child_right.state == (1, 2)
+
+    # From the right ([1, 2]) we should have three movements available: left, down, right
+    second_children = child_right.expand(problem)
+
+    assert len(second_children) == 3
+
+    # Check that the the down node is correct
+    assert second_children[0].parent == child_right
+    assert second_children[0].state == (2, 2)
+    assert second_children[0].action == 'down'
+    assert second_children[0].depth == 2
+    assert second_children[0].path_cost == 4.0 
 
 def test_goal():
     # The goal is [10][10] by default.
