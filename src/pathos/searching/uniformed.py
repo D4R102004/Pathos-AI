fd141394a0b89b335934a224993a3962b34257a1
D4R102004 <alfonsodario54@gmail.com>
2025-12-31 10:26:07 -0500

    feat(uniformed): Implemented reconsturct path function that returns a list of actions from the root to a given node
    related to #3

--- src/pathos/searching/uniformed.py
+++ src/pathos/searching/uniformed.py
@@ -3,7 +3,27 @@ Uninformed Search Algorithms.
 
 These algorithms explore the search space without any knowledge of the goal's location.
 They are 'blind' but systematic.
+BFS and DFS implementations.
 """
 
-import deque
-import Optional, List, Set, TypeVar
+from collections import deque
+from typing import Optional, List, Set, TypeVar
+from pathos.core import Node, SearchDomain, GoalOriented
+
+S = TypeVar("S")  # State
+A = TypeVar("A")  # Action
+
+def reconstruct_path(node: Node[S, A]) -> List[S]:
+    """
+    Trace back from goal to root to find the action sequence.
+    """
+    path = []
+    current = node
+    while current.parent is not None:
+        if current.action is not None:
+            path.append(current.action)
+        current = current.parent
+    
+    return list(reversed(path))
+    
+
