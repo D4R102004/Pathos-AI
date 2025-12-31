86a13facafbbb3194cdd711d152971c133a91b38
D4R102004 <alfonsodario54@gmail.com>
2025-12-31 09:29:04 -0500

    fix(test): fixed renaming lower bound and upper bound to left bound and upper bound

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,16 +9,24 @@ from pathos.core import Node, SearchDomain, GoalOriented
 # A simple "Number Line" from 0 to 10 by default: You can step Left (-1) or Right (+1).
 class NumberLine(GoalOriented[int, str]):
     def __init__(self, initial_state : int = 0, leftBound: int = 0, rightBound: int = 10):
-        self.leftBound = leftBound
-        self.rightBound = rightBound
+        self._leftBound = leftBound
+        self._rightBound = rightBound
         self._initial_state = initial_state
         
     @property
     def initial_state(self) -> int:
         return self._initial_state
     
+    @property
+    def left_bound(self) -> int:
+        return self._leftBound
+    
+    @property
+    def right_bound(self) -> int:
+        return self._rightBound
+    
     def actions(self, state: int):
-        leftBound, rightBound = self.leftBound, self.rightBound
+        leftBound, rightBound = self.left_bound, self.right_bound
         actions = []
 
         if state > leftBound:
@@ -37,7 +45,7 @@ class NumberLine(GoalOriented[int, str]):
         return state
 
     def is_goal(self, state: int) -> bool:
-        return state == self.rightBound
+        return state == self.right_bound
 
 # --- 2. The Tests ---
 
