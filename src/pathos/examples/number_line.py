9a33f592cc28eea35661c178dfd2d6b2ba5edd4f
D4R102004 <alfonsodario54@gmail.com>
2025-12-31 19:40:56 -0500

    refactor(examples): Switched the location of the numberLine from the test to a separate file in examples

---
+++ src/pathos/examples/number_line.py
@@ -0,0 +1,44 @@
+from pathos.core import Node, SearchDomain, GoalOriented
+
+# --- 1. The Dummy Problem ---
+# We create a fake problem to test the Node.
+# A simple "Number Line" from 0 to 10 by default: You can step Left (-1) or Right (+1).
+class NumberLine(GoalOriented[int, str]):
+    def __init__(self, initial_state : int = 0, leftBound: int = 0, rightBound: int = 10):
+        self._leftBound = leftBound
+        self._rightBound = rightBound
+        self._initial_state = initial_state
+        
+    @property
+    def initial_state(self) -> int:
+        return self._initial_state
+    
+    @property
+    def left_bound(self) -> int:
+        return self._leftBound
+    
+    @property
+    def right_bound(self) -> int:
+        return self._rightBound
+    
+    def actions(self, state: int):
+        leftBound, rightBound = self.left_bound, self.right_bound
+        actions = []
+
+        if state > leftBound:
+            actions.append("-1")
+        if state < rightBound:
+            actions.append("+1")
+        
+        return actions
+    
+    def result(self, state: int, action: str) -> int:
+
+        if action == "+1":
+            return state + 1
+        elif action == "-1":
+            return state - 1
+        return state
+
+    def is_goal(self, state: int) -> bool:
+        return state == self.right_bound
